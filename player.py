461060991c5cd47e6d81d455746be45eb3dbc266
Mike Ashi <mikeashi2013@gmail.com>
2020-09-14 00:47:15 +0200

    add player class

---
+++ player.py
@@ -0,0 +1,26 @@
+import random
+import chess
+
+class Player:
+    def get_next_move(self,board:chess.Board):
+        """ given a board the player should return a move """
+        pass
+    
+    def get_name(self):
+        pass
+
+
+class RandomPlayer(Player):
+    def get_next_move(self,board:chess.Board):
+        yield random.choice([m for m in board.legal_moves])
+    
+    def get_name(self):
+        return 'RandomPlayer'
+
+
+class sinnawr(Player):
+    def get_next_move(self,board):
+        yield random.choice([m for m in board.legal_moves])
+    
+    def get_name(self):
+        return 'Sinnawr'
