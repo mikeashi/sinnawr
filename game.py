2d76d98254395d4b52a27a9616a096ecc73f1cd7
Mike Ashi <mikeashi2013@gmail.com>
2020-09-14 00:54:04 +0200

    add game class

---
+++ game.py
@@ -0,0 +1,30 @@
+import chess
+import chess.pgn
+import player
+
+
+class Game:
+    def __init__(self, white:player , black:player):
+        self.board = chess.Board()
+        self.white = white
+        self.black = black
+    
+    def play(self):
+        game = chess.pgn.Game()
+        game.headers["White"] = self.white.get_name()
+        game.headers["Black"] = self.black.get_name()
+        node = game
+
+        while not self.board.is_game_over():
+            if self.board.turn:
+                move = self.white.get_next_move(self.board)
+            else:
+                move = self.black.get_next_move(self.board)
+            self.board.push(move)            
+            node = node.add_variation(move)
+
+        game.headers["Result"] = self.board.result()
+        return game
+
+    def restart(self):
+        self.board = chess.Board()
