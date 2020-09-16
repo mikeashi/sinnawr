3763280087eedeb0dd83432a225bec26eabbac54
Mike Ashi <mikeashi2013@gmail.com>
2020-09-16 04:21:08 +0200

    use negaMax

--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 import random
 import chess
+from chessNode import ChessNode
+import evaluation
 
 class Player:
     def get_next_move(self,board:chess.Board):
@@ -20,7 +22,20 @@ class RandomPlayer(Player):
 
 class Sinnawr(Player):
     def get_next_move(self,board):
-        return random.choice([m for m in board.legal_moves])
+        max = self.negaMax(ChessNode(board,None),3)
+        return max.move
     
     def get_name(self):
         return 'Sinnawr'
+
+    def negaMax(self,node:ChessNode,depth):
+        if depth == 0:
+            return ChessNode(None,None,evaluation.evaluation(node.board)) 
+        max = ChessNode(None,None)
+        for move in [m for m in node.board.legal_moves]:
+            newNode = ChessNode(node.board,move)
+            newNode.score = -1 * self.negaMax(newNode,depth-1).score
+            node.children.append(newNode)
+            if newNode.score >  max.score :
+                max = newNode
+        return max
