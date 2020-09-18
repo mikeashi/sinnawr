23aa31a7261d9e1bdce9aa0a10eb453939de8ec9
Mike Ashi <mikeashi2013@gmail.com>
2020-09-18 19:38:01 +0200

    alphabeta

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import random
 import chess
 from chessNode import ChessNode
 import evaluation
+import chess.engine
 
 class Player:
     def get_next_move(self,board:chess.Board):
@@ -19,23 +20,76 @@ class RandomPlayer(Player):
     def get_name(self):
         return 'RandomPlayer'
 
+class Stockfish(Player):
+    def __init__(self):
+        self.engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
+    
+    def get_next_move(self,board:chess.Board):
+        return self.engine.play(board, chess.engine.Limit(time=3)).move
+    
+    def get_name(self):
+        return 'Stockfish'
+
 
 class Sinnawr(Player):
-    def get_next_move(self,board):
-        max = self.negaMax(ChessNode(board,None),3)
-        return max.move
-    
+
+    def get_next_move(self,board:chess.Board):
+        bestMove = chess.Move.null()
+        bestValue = -99999
+        alpha = -100000
+        beta = 100000
+        for move in board.legal_moves:
+            board.push(move)
+            boardValue = -self.alphabeta(board,-beta, -alpha, 3)
+            if boardValue > bestValue:
+                bestValue = boardValue;
+                bestMove = move
+            if( boardValue > alpha ):
+                alpha = boardValue
+            board.pop()
+        return bestMove
+
     def get_name(self):
         return 'Sinnawr'
 
-    def negaMax(self,node:ChessNode,depth):
-        if depth == 0:
-            return ChessNode(None,None,evaluation.evaluation(node.board)) 
-        max = ChessNode(None,None)
-        for move in [m for m in node.board.legal_moves]:
-            newNode = ChessNode(node.board,move)
-            newNode.score = -1 * self.negaMax(newNode,depth-1).score
-            node.children.append(newNode)
-            if newNode.score >  max.score :
-                max = newNode
-        return max
+    def alphabeta(self,board:chess.Board, alpha, beta, depthleft ):
+        bestscore = float('-inf')
+        if( depthleft == 0 ):
+            #return evaluation.evaluation(board)
+            return self.quiesce(board, alpha, beta, 3)
+            
+        for move in board.legal_moves:
+            board.push(move)   
+            score = -self.alphabeta( board,-beta, -alpha, depthleft - 1 )
+            board.pop()
+            if score >= beta:
+                return score
+            if score > bestscore:
+                self.bestMove = move
+                bestscore = score
+            if score > alpha:
+                alpha = score     
+        return bestscore
+    
+    def quiesce(self,board:chess.Board, alpha, beta, depthleft):
+        stand_pat = evaluation.evaluation(board)
+        if depthleft == 0 :
+            return stand_pat
+        if( stand_pat >= beta ):
+            return beta
+        if( alpha < stand_pat ):
+            alpha = stand_pat
+
+        for move in board.legal_moves:
+            if board.is_capture(move):
+                board.push(move)
+                score = -self.quiesce(board,-beta, -alpha ,depthleft -1)
+                board.pop()
+
+                if score >= beta:
+                    return beta
+                if score > alpha:
+                    alpha = score  
+        return alpha
+    
+
